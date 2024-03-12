354e8053397cd6cfaffe7e1af5cc7642060ae118
joffei2 <joffei112@gmail.com>
2024-03-12 16:18:30 +0000

    ...

--- 0x01-python-if_else_loops_functions/list.h
+++ 0x01-python-if_else_loops_functions/list.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * struct list int_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
@@ -13,8 +13,8 @@
 
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
