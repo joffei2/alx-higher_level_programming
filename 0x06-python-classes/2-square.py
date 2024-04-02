1c942b24fc72a51685cfd9f52027001800630152
unknown <you@example.com>
2024-04-02 16:24:14 +0100

    ...

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -8,7 +7,6 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square.
-
         Args:
             size (int): The size of the new square.
         """
@@ -17,4 +15,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
