89dee641292cba03ff707aa192ebefab82008149
unknown <you@example.com>
2024-05-23 19:22:05 +0000

    ...

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- the
 SELECT `title`, SUM(`rate`) AS `rating`
-    FROM `tv_shows` AS t
-        INNER JOIN `tv_shows_rating` AS r
-        ON t.`id` = r.`shows_id`
+FROM `tv_shows` AS t
+    INNER JOIN `tv_shows_rating` AS r
+    ON t.`id` = r.`shows_id`
 GROUP BY `title`
 ORDER BY `rating` DESC;
