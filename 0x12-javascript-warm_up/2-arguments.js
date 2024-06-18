35222c251957e6b7b42ef4504ff7e5817c73760e
joffei2 <joffei112@gmail.com>
2024-06-18 22:31:32 +0000

    fixed some errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv;
 if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
-  console.log('argument found');
+  console.log('Argument found');
 } else {
-  console.log('arguments found');
+  console.log('Arguments found');
 }
