cdaa3cdf81867a30884015fe2634b773861aa9e3
joffei2 <joffei112@gmail.com>
2024-06-19 15:01:31 +0000

    ...

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 /*  a class Rectangle that defines a rectangle with arg */
 
+class Rectangle {
+    constructor(w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
