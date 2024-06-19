cc185076dd18a98fff4ff27962bf9ce7d763561e
joffei2 <joffei112@gmail.com>
2024-06-19 12:31:08 +0000

    ...

---
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*  a class Rectangle that defines a rectangle with args */
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w,
+    this.height = h;
+   }
+}#!/usr/bin/node
+/*  a class Rectangle that defines a rectangle with args */
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w,
+    this.height = h;
+   }
+}
