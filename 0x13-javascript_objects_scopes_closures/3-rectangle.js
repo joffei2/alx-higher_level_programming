4a8a4396ce0b41e0029f222304685617543a1b0c
joffei2 <joffei112@gmail.com>
2024-06-20 14:22:46 +0000

    ...

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,16 @@
 #!/usr/bin/node
 /* ... */
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width = 0;
-      this.height = 0;
     }
   }
-
   print () {
-    if (this.width === 0 || this.height === 0) {
-      return;
-    }
-
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-}
+};
