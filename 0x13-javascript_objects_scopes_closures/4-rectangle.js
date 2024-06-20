7766a713eecd2430317728cf25104ee9ee55680c
joffei2 <joffei112@gmail.com>
2024-06-20 14:28:02 +0000

    ...

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* a class Rectangle that defines a rectangle
+ take 2 arguments: w and h */
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+    rotate () {
+      let tmp = this.width;
+      this.width = this.height;
+      this.height = tmp;
+    }
+    double () {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  };
