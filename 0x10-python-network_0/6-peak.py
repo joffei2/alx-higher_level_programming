c26f933efcc91f7b172afb2a7360f2495c89b551
joffei2 <joffei112@gmail.com>
2024-07-04 22:00:57 +0000

    ...

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+""" Finds a peak in a list of unsorted integers"""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int or None: The peak element in the list, \
+                or None if the list is empty.
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] <= list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
